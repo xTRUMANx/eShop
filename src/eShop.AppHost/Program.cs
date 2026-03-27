0fb40cd089d8b735517cdee1cddf574c607e757d
Mustafa Saeed <me@mustafasha.com>
2026-03-27 14:05:41 +0300

    Setup Docker Compose

--- src/eShop.AppHost/Program.cs
+++ src/eShop.AppHost/Program.cs
@@ -2,15 +2,37 @@
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var compose = builder.AddDockerComposeEnvironment("compose")
+    .ConfigureEnvFile(env =>
+    {
+        env["REGISTRY"] = new()
+        {
+            Name = "REGISTRY",
+            DefaultValue = "localhost:5000",            
+        };
+    });
+
 builder.AddForwardedHeaders();
 
-var redis = builder.AddRedis("redis");
+var redis = builder.AddRedis("redis")
+    .PublishAsDockerComposeService((resource, service) =>
+    {
+        service.Name = "redis";
+    });
 var rabbitMq = builder.AddRabbitMQ("eventbus")
-    .WithLifetime(ContainerLifetime.Persistent);
+    .WithLifetime(ContainerLifetime.Persistent)
+    .PublishAsDockerComposeService((resource, service) =>
+    {
+        service.Name = "eventbus";
+    });
 var postgres = builder.AddPostgres("postgres")
     .WithImage("ankane/pgvector")
     .WithImageTag("latest")
-    .WithLifetime(ContainerLifetime.Persistent);
+    .WithLifetime(ContainerLifetime.Persistent)
+    .PublishAsDockerComposeService((resource, service) =>
+    {
+        service.Name = "postgres";
+    });
 
 var catalogDb = postgres.AddDatabase("catalogdb");
 var identityDb = postgres.AddDatabase("identitydb");
@@ -23,48 +45,84 @@ var launchProfileName = ShouldUseHttpForEndpoints() ? "http" : "https";
 var identityApi = builder.AddProject<Projects.Identity_API>("identity-api", launchProfileName)
     .WithExternalHttpEndpoints()
     .WithReference(identityDb)
-    .WithHttpHealthCheck("/health");
+    .WithHttpHealthCheck("/health")
+    .PublishAsDockerComposeService((resource, service) =>
+    {
+        service.Name = "identity-api";
+    });
 
 var identityEndpoint = identityApi.GetEndpoint(launchProfileName);
 
 var basketApi = builder.AddProject<Projects.Basket_API>("basket-api")
     .WithReference(redis)
     .WithReference(rabbitMq).WaitFor(rabbitMq)
-    .WithEnvironment("Identity__Url", identityEndpoint);
+    .WithEnvironment("Identity__Url", identityEndpoint)
+    .PublishAsDockerComposeService((resource, service) =>
+    {
+        service.Name = "basket-api";
+    });
 redis.WithParentRelationship(basketApi);
 
 var catalogApi = builder.AddProject<Projects.Catalog_API>("catalog-api")
     .WithReference(rabbitMq).WaitFor(rabbitMq)
-    .WithReference(catalogDb);
+    .WithReference(catalogDb)
+    .PublishAsDockerComposeService((resource, service) =>
+    {
+        service.Name = "catalog-api";
+    });
 
 var orderingApi = builder.AddProject<Projects.Ordering_API>("ordering-api")
     .WithReference(rabbitMq).WaitFor(rabbitMq)
     .WithReference(orderDb).WaitFor(orderDb)
     .WithHttpHealthCheck("/health")
-    .WithEnvironment("Identity__Url", identityEndpoint);
+    .WithEnvironment("Identity__Url", identityEndpoint)
+    .PublishAsDockerComposeService((resource, service) =>
+    {
+        service.Name = "ordering-api";
+    });
 
 builder.AddProject<Projects.OrderProcessor>("order-processor")
     .WithReference(rabbitMq).WaitFor(rabbitMq)
     .WithReference(orderDb)
-    .WaitFor(orderingApi); // wait for the orderingApi to be ready because that contains the EF migrations
+    .WaitFor(orderingApi)
+    .PublishAsDockerComposeService((resource, service) =>
+    {
+        service.Name = "order-processor";
+    }); // wait for the orderingApi to be ready because that contains the EF migrations
 
 builder.AddProject<Projects.PaymentProcessor>("payment-processor")
-    .WithReference(rabbitMq).WaitFor(rabbitMq);
+    .WithReference(rabbitMq).WaitFor(rabbitMq)
+    .PublishAsDockerComposeService((resource, service) =>
+    {
+        service.Name = "payment-processor";
+    });
 
 var webHooksApi = builder.AddProject<Projects.Webhooks_API>("webhooks-api")
     .WithReference(rabbitMq).WaitFor(rabbitMq)
     .WithReference(webhooksDb)
-    .WithEnvironment("Identity__Url", identityEndpoint);
+    .WithEnvironment("Identity__Url", identityEndpoint)
+    .PublishAsDockerComposeService((resource, service) =>
+    {
+        service.Name = "webhooks-api";
+    });
 
 // Reverse proxies
 builder.AddYarp("mobile-bff")
     .WithExternalHttpEndpoints()
-    .ConfigureMobileBffRoutes(catalogApi, orderingApi, identityApi);
+    .ConfigureMobileBffRoutes(catalogApi, orderingApi, identityApi)
+    .PublishAsDockerComposeService((resource, service) =>
+    {
+        service.Name = "mobile-bff";
+    });
 
 // Apps
 var webhooksClient = builder.AddProject<Projects.WebhookClient>("webhooksclient", launchProfileName)
     .WithReference(webHooksApi)
-    .WithEnvironment("IdentityUrl", identityEndpoint);
+    .WithEnvironment("IdentityUrl", identityEndpoint)
+    .PublishAsDockerComposeService((resource, service) =>
+    {
+        service.Name = "webhooksclient";
+    });
 
 var webApp = builder.AddProject<Projects.WebApp>("webapp", launchProfileName)
     .WithExternalHttpEndpoints()
@@ -74,7 +132,11 @@ var webApp = builder.AddProject<Projects.WebApp>("webapp", launchProfileName)
     .WithReference(orderingApi)
     .WithReference(rabbitMq).WaitFor(rabbitMq)
     .WaitFor(identityApi)
-    .WithEnvironment("IdentityUrl", identityEndpoint);
+    .WithEnvironment("IdentityUrl", identityEndpoint)
+    .PublishAsDockerComposeService((resource, service) =>
+    {
+        service.Name = "webapp";
+    });
 
 // set to true if you want to use OpenAI
 bool useOpenAI = false;
@@ -99,6 +161,12 @@ identityApi.WithEnvironment("BasketApiClient", basketApi.GetEndpoint("http"))
            .WithEnvironment("WebhooksApiClient", webHooksApi.GetEndpoint("http"))
            .WithEnvironment("WebhooksWebClient", webhooksClient.GetEndpoint(launchProfileName))
            .WithEnvironment("WebAppClient", webApp.GetEndpoint(launchProfileName));
+
+// builder.AddDockerComposeEnvironment("compose")
+//        .WithProperties(env =>
+//        {
+//            env.DashboardEnabled = true;
+//        });
 
 builder.Build().Run();
 
